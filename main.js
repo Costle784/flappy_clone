44d1ac35f11139a34712856cbed75436095b2010
Ammar Alasadi <alasadi.ammar@gmail.com>
2017-05-24 14:23:08 -0400

    dog jumping

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
+var game = new Phaser.Game(800,560);
 console.log('connected')
-
 var mainState = {
   preload: function() {
     game.load.image('dog', 'assets/dog.png')
@@ -10,39 +10,41 @@ var mainState = {
   create: function (){
 
     game.add.sprite(0,0,'background').scale.setTo(2,2);
-
-    var dog = game.add.sprite(100, 400, 'dog').scale.setTo(0.1,0.1);
+    game.stage.backgroundColor = '#71c5cf';
 
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
-    game.physics.arcade.enable(dog);
+    this.dog = game.add.sprite(100, 400, 'dog')
+    this.dog.scale.setTo(0.1,0.1);
 
-    // this.dog.body.gravity.y = 1000;
+
+    game.physics.arcade.enable(this.dog);
+    console.log(this.dog)
+    this.dog.body.gravity.y = 1000;
 
     var spaceKey = game.input.keyboard.addKey(
       Phaser.Keyboard.SPACEBAR);
       spaceKey.onDown.add(this.jump, this);
     },
 
-  update: function() {
-    if (this.dog.y < 0 || this.dog.y > 560)
-        this.restartGame();
+    update: function() {
+      if (this.dog.y < 0 || this.dog.y > 560)
+      this.restartGame();
     },
-      jump: function() {
-        // Add a vertical velocity to the bird
-        this.bird.body.velocity.y = -350;
+    jump: function() {
+      // Add a vertical velocity to the bird
+      this.dog.body.velocity.y = -350;
     },
 
     restartGame: function() {
-        // Start the 'main' state, which restarts the game
-        game.state.start('main');
+      // Start the 'main' state, which restarts the game
+      game.state.start('main');
     },
   };
 
 
 
-var game = new Phaser.Game(800,560);
 
-game.state.add('main', mainState);
+  game.state.add('main', mainState);
 
-game.state.start('main')
+  game.state.start('main')
