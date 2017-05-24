fbed72c567886c6c09dbb8cc803a09c33377db46
Curtis Ostle <curtisostle@gmail.com>
2017-05-24 13:02:50 -0400

    dog physics

--- main.js
+++ main.js
@@ -2,20 +2,46 @@ console.log('connected')
 
 var mainState = {
   preload: function() {
-    game.load.image('bird', 'assets/bird.png')
+    game.load.image('dog', 'assets/dog.png')
+    game.load.image('background', 'assets/background.jpg')
+    game.load.image('bone', 'assets/bone.png')
   },
 
   create: function (){
 
-  },
+    game.add.sprite(0,0,'background').scale.setTo(2,2);
+
+    var dog = game.add.sprite(100, 400, 'dog').scale.setTo(0.1,0.1);
+
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+
+    game.physics.arcade.enable(dog);
+
+    // this.dog.body.gravity.y = 1000;
+
+    var spaceKey = game.input.keyboard.addKey(
+      Phaser.Keyboard.SPACEBAR);
+      spaceKey.onDown.add(this.jump, this);
+    },
 
   update: function() {
+    if (this.dog.y < 0 || this.dog.y > 560)
+        this.restartGame();
+    },
+      jump: function() {
+        // Add a vertical velocity to the bird
+        this.bird.body.velocity.y = -350;
+    },
 
-  }
+    restartGame: function() {
+        // Start the 'main' state, which restarts the game
+        game.state.start('main');
+    },
+  };
 
-};
 
-var game = new Phaser.Game(400,490);
+
+var game = new Phaser.Game(800,560);
 
 game.state.add('main', mainState);
 
